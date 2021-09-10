35b51a3a6ecc5f5912118e705c348f462abbbfe8
winchell459 <johncmorris@gmail.com>
2021-09-10 14:59:01 -0700

    end of day 9/10/21

---
+++ Assets/Scripts/Gerrymandering.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Gerrymandering : MonoBehaviour
+{
+    public Clingo.ClingoSolver Solver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartJob();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartJob()
+    {
+        string aspCode = GerrymanderingASP.GetASP();
+        string filename = Clingo.ClingoUtil.CreateFile(aspCode);
+        Solver.Solve(filename);
+    }
+}
