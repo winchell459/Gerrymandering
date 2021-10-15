583bacabc79271cbb49aa327d7cc1dbbc3aeda5f
winchell459 <johncmorris@gmail.com>
2021-10-14 18:53:08 -0700

    Hard coded map for blocks

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -22,39 +22,75 @@ public class Map : MonoBehaviour
 
         map = new MapPixel[width, height];
 
-        foreach(List<string> island in answerset["island"])
+        foreach (List<string> island in answerset["block"])
         {
-            int x = int.Parse(island[1]) - 1;
+            int x = int.Parse(island[0]) - 1;
             int y = int.Parse(island[2]) - 1;
             Color type = Color.white;
 
-            switch (island[0])
+            switch (island[3])
             {
-                case "purple":
+                case "resource":
                     type = Color.magenta;
                     break;
-                case "green":
+                case "grass":
                     type = Color.green;
                     break;
-                case "blue":
+                case "water":
                     type = Color.blue;
                     break;
-                case "orange":
+                case "sand":
                     type = new Color(255 / 255f, 165 / 255f, 0);
                     break;
-                case "red":
+                case "tree":
                     type = Color.red;
+                    break;
+                case "food":
+                    type = Color.yellow;
                     break;
                 default:
 
                     break;
             }
-          
+
             MapPixel pixel = Instantiate(PixelPrefab, transform).GetComponent<MapPixel>();
             pixel.SetPixel(x * PixelSpacing, y * PixelSpacing, type);
             pixel.AddNote(island);
         }
-        AdjustCamera();
+
+        //foreach(List<string> island in answerset["island"])
+        //{
+        //    int x = int.Parse(island[1]) - 1;
+        //    int y = int.Parse(island[2]) - 1;
+        //    Color type = Color.white;
+
+        //    switch (island[0])
+        //    {
+        //        case "purple":
+        //            type = Color.magenta;
+        //            break;
+        //        case "green":
+        //            type = Color.green;
+        //            break;
+        //        case "blue":
+        //            type = Color.blue;
+        //            break;
+        //        case "orange":
+        //            type = new Color(255 / 255f, 165 / 255f, 0);
+        //            break;
+        //        case "red":
+        //            type = Color.red;
+        //            break;
+        //        default:
+
+        //            break;
+        //    }
+
+        //    MapPixel pixel = Instantiate(PixelPrefab, transform).GetComponent<MapPixel>();
+        //    pixel.SetPixel(x * PixelSpacing, y * PixelSpacing, type);
+        //    pixel.AddNote(island);
+        //}
+        //AdjustCamera();
     }
     //DisplayMap() END
 
